a507c2f6e57eee6ffa89932df6fd64dbe854d879
AbbasDevs <abbas.dev98@gmail.com>
2025-12-29 22:23:17 +0200

    section 5

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -10,9 +10,34 @@ public class BaseSpecification<T>(Expression<Func<T,bool>>? critetria):
     public Expression <Func<T,object>>?OrderBy {get; private set;}
      public Expression<Func<T, object>>? OrderByDescending { get; private set; }
     public bool IsDistinct {get; private set;}
+
+        public int Take { get; private set; }
+
+    public int Skip { get; private set; }
+
+    public bool IsPagingEnabled { get; private set; }
+
+    public IQueryable<T> ApplyCriteria(IQueryable<T> query)
+    {
+        if (Criteria != null)
+        {
+            query = query.Where(Criteria);
+        }
+
+        return query;
+    }
+
     protected void AddOrderBy(Expression<Func<T,object>> orderByExpression)
     {
         OrderBy=orderByExpression;
+    }
+
+
+    protected void ApplyPaging(int skip, int take)
+    {
+        Skip = skip;
+        Take = take;
+        IsPagingEnabled = true;
     }
     protected void AddOrderByDescending(Expression<Func<T,object>> orderByDescExpression)
     {
